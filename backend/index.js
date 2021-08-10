6bc3c69fb181492c2c2b1c62966ab6eecebed880
Drayerr <gabrielgrv2015@outlook.com>
2021-08-10 16:02:19 -0300

    Cleaning up code.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 const xlsx = require('xlsx')
 
-const myFile = 'FOR 609.xlsx'
+//Insira o nome e extensão do arquivo que deseja copiar. Obs ele lê a partir da raiz.
+const myFile = 'FOR 0219.xlsx' 
 
-const wb = xlsx.readFile(myFile)  //Lê o arquivo xlsx armazenado em 'myFile' e armazena as informações em 'wb'
+//Nome da aba que deseja copiar
+const mySheet= 'Ficha de Campo e Calibração' 
 
-const newWB = xlsx.utils.book_new('NEW WORKBOOK') //Cria uma nova pasta de trabalho
-const newWS = wb.Sheets['Carta de Controle Pd']                  //Pega as informações da aba 'Plan1' do arquivo que foi lido e insere em 'newWS'
+//Lê o arquivo xlsx armazenado em 'myFile' e armazena as informações em 'wb'
+const workBook = xlsx.readFile(myFile)
 
-xlsx.utils.book_append_sheet(newWB, newWS, 'Recovery')  //Acrescenta a nova Worksheet(newWS) com o nome 'Recovery' na Workbook(newWB)
+//Cria uma nova pasta de trabalho
+const newWB = xlsx.utils.book_new('NEW WORKBOOK')
+const newWS = workBook.Sheets[mySheet]
 
-console.log(wb.SheetNames);
-xlsx.writeFile(newWB, 'recovery 2.xlsx')      //Gera um novo arquivo com a pasta de trabalho 'newWB' com o nome de 'recovery'
+//Acrescenta a nova WorkSheet(newWS) com o nome 'Recovery' na WorkBook(newWB)
+xlsx.utils.book_append_sheet(newWB, newWS, 'Recovery') 
+
+//Gera um arquivo com a pasta de trabalho 'newWB' com o nome 'RecoveredFile.xlsx'
+xlsx.writeFile(newWB, 'RecoveredFile ' + Date.now()+ '.xlsx') 
+
+console.log('---XLSX-JS---');
+console.log('A Aba (' + mySheet + ') foi copiada para um novo arquivo.')
